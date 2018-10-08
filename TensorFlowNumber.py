077adaecbb35d267c0bc5dd5ce9b1207a0de6ab4
stuartnankai <jians@kth.se>
2018-10-08 15:08:05 +0200

    Load data and build CNN model

--- TensorFlowNumber.py
+++ TensorFlowNumber.py
@@ -3,8 +3,6 @@ import matplotlib.pyplot as plt
 
 import numpy as np
 
-
-
 mnist = tf.keras.datasets.mnist
 
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
@@ -35,7 +33,7 @@ predictions = model.predict(x_test)
 print(y_test)
 print(np.argmax(predictions[1]))
 
-plt.imshow(x_test[0],cmap=plt.cm.binary)
+plt.imshow(x_test[0], cmap=plt.cm.binary)
 plt.show()
 
 # plt.imshow(x_train[1])
