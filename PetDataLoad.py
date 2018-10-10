6729228a1d67c4d96b00069165701ca7b8a72fe4
stuartnankai <jians@kth.se>
2018-10-10 09:36:43 +0200

    Using tensorboard in order to check overfitting

--- PetDataLoad.py
+++ PetDataLoad.py
@@ -4,8 +4,16 @@ from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten
 from tensorflow.keras.layers import Conv2D, MaxPooling2D
-
+from tensorflow.keras.callbacks import TensorBoard
 import pickle
+import time
+
+
+NAME = "Cats-vs-dogs-CNN-2".format(int(time.time()))
+
+tensorboard = TensorBoard(log_dir="logs/{}".format(NAME))
+
+
 
 pickle_in = open("X.pickle","rb")
 X = pickle.load(pickle_in)
@@ -21,24 +29,27 @@ X = X/255.0
 
 model = Sequential()
 
-model.add(Conv2D(256, (3, 3), input_shape=X.shape[1:]))
+model.add(Conv2D(64, (3, 3), input_shape=X.shape[1:]))
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 
-model.add(Conv2D(256, (3, 3)))
+model.add(Conv2D(64, (3, 3)))
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 
 model.add(Flatten())  # this converts our 3D feature maps to 1D feature vectors
-
 model.add(Dense(64))
+model.add(Activation("relu"))
+
 
 model.add(Dense(1))
 model.add(Activation('sigmoid'))
+
+tensorboard = TensorBoard(log_dir="logs/{}".format(NAME))
 
 model.compile(loss='binary_crossentropy',
               optimizer='adam',
               metrics=['accuracy'])
 
-model.fit(X, y, batch_size=32, epochs=3, validation_split=0.3)
+model.fit(X, y, batch_size=32, epochs=10, validation_split=0.3,callbacks=[tensorboard])
 
