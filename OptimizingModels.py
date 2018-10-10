4df62d66acaa1825158a925cfd74768c640f0a20
stuartnankai <jians@kth.se>
2018-10-10 11:38:48 +0200

    Find the best model and save it

---
+++ OptimizingModels.py
@@ -0,0 +1,64 @@
+import time
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense,Dropout,Activation,Flatten
+from tensorflow.keras.layers import Conv2D,MaxPooling2D
+from tensorflow.keras.callbacks import TensorBoard
+import pickle
+
+pickle_in= open("X.pickle","rb")
+X = pickle.load(pickle_in)
+
+
+pickle_in= open("y.pickle","rb")
+y = pickle.load(pickle_in)
+
+X = X/255.0
+
+dense_layers = [1]
+layer_sizes = [32]
+conv_layers = [1]
+
+
+for dense_layer in dense_layers:
+    for layer_size in layer_sizes:
+        for conv_layer in conv_layers:
+            NAME = "{}-conv-{}-nodes-{}-dense-{}-Prediction".format(conv_layer, layer_size, dense_layer, int(time.time()))
+            print(NAME)
+
+            model = Sequential()
+
+            model.add(Conv2D(layer_size, (3, 3), input_shape=X.shape[1:]))
+            model.add(Activation('relu'))
+            model.add(MaxPooling2D(pool_size=(2, 2)))
+
+            for l in range(conv_layer-1):
+                model.add(Conv2D(layer_size, (3, 3)))
+                model.add(Activation('relu'))
+                model.add(MaxPooling2D(pool_size=(2, 2)))
+
+            model.add(Flatten())
+
+            for _ in range(dense_layer):
+                model.add(Dense(layer_size))
+                model.add(Activation('relu'))
+
+            model.add(Dense(1))
+            model.add(Activation('sigmoid'))
+
+            tensorboard = TensorBoard(log_dir="logs/{}".format(NAME))
+
+            model.compile(loss='binary_crossentropy',
+                          optimizer='adam',
+                          metrics=['accuracy'],
+                          )
+
+            model.fit(X, y,
+                      batch_size=32,
+                      epochs=5,
+                      validation_split=0.3,
+                      callbacks=[tensorboard])
+
+# 1-32-1 will get the best result
+
+model.save("1*32*1-CNN.model")
+
